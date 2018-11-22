cc2e5def0c94dc71bfa81c0ce04382ae66d25db8
Jackson Raffety <jacksonraffety@gmail.com>
2018-11-21 22:26:29 -0500

    Update server.py

--- server.py
+++ server.py
@@ -254,8 +254,8 @@ def add():
   state = request.form['State']
   country = request.form['Country']
   fullname = fname + " " + lname
-  g.conn.execute('INSERT INTO customer VALUES (cid,tel_num,full_name,state,city,zip,address)', \
-                cid,telno,fullname,state,city,zip_code,address)
+  g.conn.execute('INSERT INTO customer VALUES (%s,%s,%s,%s,%s,%s,%s)' \
+                % (cid,telno,fullname,state,city,zip_code,address))
   return redirect('/')
 
 
