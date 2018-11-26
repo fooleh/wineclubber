0268cf8a45d12ee5960ddbb75a1d205debe86ec9
Jackson Raffety <jacksonraffety@gmail.com>
2018-11-26 13:23:11 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python2.7
 
 """
-Columbia's COMS W4111.001 Introduction to Databases
-Example Webserver
-
-To run locally:
-
-    python server.py
-
-Go to http://localhost:8111 in your browser.
-
-A debugger such as "pdb" may be helpful for debugging.
-Read about it online.
+jackson raffety
+jwr2131
+hw1.3
 """
 
 import os
@@ -24,36 +16,9 @@ from random import randint
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
-
-#
-# The following is a dummy URI that does not connect to a valid database. You will need to modify it to connect to your Part 2 database in order to use the data.
-#
-# XXX: The URI should be in the format of: 
-#
-#     postgresql://USER:PASSWORD@104.196.18.7/w4111
-#
-# For example, if you had username biliris and password foobar, then the following line would be:
-#
-#     DATABASEURI = "postgresql://biliris:foobar@104.196.18.7/w4111"
-#
 DATABASEURI = "postgresql://jwr2131:cde34rfvCDE#$RFV@35.196.158.126/proj1part2"
 
-
-#
-# This line creates a database engine that knows how to connect to the URI above.
-#
 engine = create_engine(DATABASEURI)
-
-#
-# Example of running queries in your database
-# Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
-#
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
-
 
 @app.before_request
 def before_request():
@@ -82,31 +47,9 @@ def teardown_request(exception):
   except Exception as e:
     pass
 
-
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
 @app.route('/')
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
-
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
+ 
   cursor = g.conn.execute("SELECT DISTINCT bottle_count,frequency, price FROM signed_up")
   names = []
   for row in cursor:
@@ -115,14 +58,6 @@ def index():
   context = dict(data = names)
   return render_template("index.html", **context)
 
-#
-# This is an example of a different path.  You can see it at:
-# 
-#     localhost:8111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
 @app.route('/manager')
 def manager():
   return render_template("manager.html")
@@ -206,8 +141,7 @@ def tastingrooms():
   cursor.close()
   context = dict(data = names)  
   return render_template("tastingrooms.html", **context)                                      
-                                              
-# Example of adding new data to the database                        
+                                                                    
 @app.route('/add', methods=['POST'])
 def add():
   cid = randint(10000,99999)
@@ -231,7 +165,7 @@ def add():
   if frequency == 'Bi-Annual':
     price = 300 if bottlecount == 6 else 450
   g.conn.execute('INSERT INTO signed_up(bottle_count,frequency,since,price,cid) VALUES (%s,%s,%s,%s,%s)', \
-                 (bottlecount,frequency,date_now,price,cid))
+                 (bottlecount,frequency.lower(),date_now,price,cid))
   
   return redirect('/')
 
