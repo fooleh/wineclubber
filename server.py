efd0a56f261ba253496b29f670cca8b26bcbf73b
Jackson Raffety <jacksonraffety@gmail.com>
2018-11-26 10:54:54 -0500

    Update server.py

--- server.py
+++ server.py
@@ -106,48 +106,13 @@ def index():
 
   See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
   """
-  
-  # DEBUG: this is debugging code to see what request looks like
-  #print request.args
-
-
-  #
-  # example of a database query
-  #
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  #context = dict(data = names)
-
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html")
+  cursor = g.conn.execute("SELECT DISTINCT frequency, price FROM signed_up")
+  names = []
+  for row in cursor:
+    names.append(row) 
+  cursor.close()
+  context = dict(data = names)
+  return render_template("index.html", **context)
 
 #
 # This is an example of a different path.  You can see it at:
